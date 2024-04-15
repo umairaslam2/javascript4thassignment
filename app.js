b4c6a9396c3ba242a16f8819c81f27b946619b2f
umairaslam2 <154333213+umairaslam2@users.noreply.github.com>
2024-04-15 20:34:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,164 @@
+//""
+//h
+//g
+
+//# 13-15 ;
+
+// question 1;
+
+//var arr = [];
+
+//console.log(arr);
+
+// question 2;
+
+//var arr = []
+
+//console.log(arr)
+
+// question 3;
+
+//var arr = ["My name is Umair","Salam"]
+
+//console.log(arr);
+
+// questio 4;
+
+//var arr = [1,2,3,4,5,6,7,8,9,10]
+
+//console.log(arr);
+
+// question 5;
+
+//var arr = [true,false]
+
+//console.log(arr)
+
+// question 6;
+
+//var arr = [1,3,"Salam",true,false]
+
+//console.log(arr)
+
+// question 7;
+
+//var arr = ["ACCA","CA","CMA","ICMA","BSC","B.COM"]
+
+//console.log(arr)
+
+// QUESTION 8;
+
+//var studentNames = ["Umair","Aslam","Umer"]
+//var studentScore = [420,350,290]
+//var totalMarks = 500;
+
+
+//var finalScore = "Score of" + studentNames[0] + " is" + studentScore[0] + " percentae:" + ((studentScore[0]/totalMarks)*100) + "%" + (`br`) + "Score of" + studentNames[1] + " is" + studentScore[1] + " percentae:" + ((studentScore[1]/totalMarks)*100) + "%" + "</br>" + "Score of" + studentNames[2] + " is" + studentScore[2] + " percentae:" + ((studentScore[2]/totalMarks)*100) + "%" 
+
+
+//console.log(finalScore)
+
+
+
+
+// question 9;
+
+   //A
+
+//var Color = ["Red","Yellow","Pink"];
+//arr.unshift("Blue");
+
+//console.log(arr)
+
+//B;
+
+// var arr = ["Red","Blue","Yellow"];
+// arr.push("Pink");
+
+// console.log(arr)
+
+// c;
+
+// var arr = ["Red","Blue","Yellow"];
+// arr.unshift("Pink","Black");
+
+// console.log(arr)
+
+//e;
+
+//var arr = ["Red","Blue","Yellow"];
+//arr.pop(2);
+
+//console.log(arr)
+
+//d;
+
+//var arr = ["Red","Blue","Yellow"];
+//arr.shift(0);
+
+//console.log(arr)
+
+//f;
+
+//var arr = ["Red","Blue","Yellow"];
+//arr.splice(1,0,"Black");
+
+//console.log(arr)
+
+//g;
+
+
+//var arr = ["Red","Blue","Yellow"];
+//arr.splice(1,1);
+
+//console.log(arr)
+
+
+// question 10;
+
+//var arr = [230,320,120,210];
+
+//arr.sort();
+
+//console.log(arr)
+
+// question 11;
+
+//var cities = ["Multan","Faislabad","Islamabad","Quetta"]
+
+//console.log(cities[2],cities[3])
+
+
+// question 12;
+
+//var arr = ["My","Name","is","Umair","Aslam"]
+
+//arr.join();
+
+//console.log(arr)
+
+// question 13;
+
+// var arr = []
+
+// arr.push("keybord");
+// arr.push("mouse");
+// arr.push("monitor");
+// arr.push("printer")
+
+// console.log(arr)
+
+// question 14;
+
+
+// var arr = []
+
+// arr.unshift("keybord");
+// arr.unshift("mouse");
+// arr.unshift("monitor");
+// arr.unshift("printer")
+
+// console.log(arr)
+
+
+
